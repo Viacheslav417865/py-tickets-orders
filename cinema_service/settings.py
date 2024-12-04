7922a6e3a1cc18cd66c3eb01895f461621d3c4c9
Viacheslav417865 <sfinks417865@gmail.com>
2024-12-04 13:38:13 +0200

    Updated settings and other files

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,11 +137,4 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS":
         "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 2,
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
 }
